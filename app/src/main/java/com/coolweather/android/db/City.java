7128afe16d3384ac02e9ca9562c0481c0fa2f8d9
yw1053 <1972732700@qq.com>
2020-04-02 08:47:55 +0000

    commit province city county data

--- app/src/main/java/com/coolweather/android/db/City.java
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -6,8 +6,16 @@ public class City extends DataSupport {
     private int id;
     private String cityName;
     private int cityCode;
-    private int provinceCode;
+    private int provinceId;
 
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
 
     public int getId() {
         return id;
@@ -33,11 +41,4 @@ public class City extends DataSupport {
         this.cityCode = cityCode;
     }
 
-    public int getProvinceCode() {
-        return provinceCode;
-    }
-
-    public void setProvinceCode(int provinceCode) {
-        this.provinceCode = provinceCode;
-    }
 }
